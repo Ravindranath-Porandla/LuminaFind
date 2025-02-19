d7a23e74bb3e2fa159a1c469c5b00d89da01a962
Ravindranath-Porandla <ravidranathporandla@gmail.com>
2025-02-19 12:52:47 +0530

    Initial commit

---
+++ src/main/java/com/ravenous/LuminaFind/LuminaFindApplication.java
@@ -0,0 +1,13 @@
+package com.ravenous.LuminaFind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminaFindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuminaFindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravenous/LuminaFind/LuminaFindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravenous.LuminaFind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminaFindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
